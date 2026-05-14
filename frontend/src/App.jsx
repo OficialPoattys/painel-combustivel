c3cc67518958543cf1a40dafcfb1e2bcc15c8b47
Poattys <poattysdev@gmail.com>
2026-05-14 04:39:19 -0300

    feat: adiciona Vercel Analytics e Speed Insights

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
@@ -7,6 +9,8 @@ import Metodologia from './pages/Metodologia'
 function App() {
   return (
     <BrowserRouter>
+      <Analytics />
+      <SpeedInsights />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
